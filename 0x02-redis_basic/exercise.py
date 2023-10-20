c1f50aedbdd90e96b1050296e5771f8b668ac65f
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-10-20 15:38:43 +0100

    Define a call_history decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,20 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    input_key = method.__qualname__ + ":inputs"
+    output_key = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args):
+        self._redis.rpush(input_key, *args)
+        result = method(self, str(*args))
+        self._redis.rpush(output_key, str(result))
+        return result
+
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         """
@@ -31,6 +45,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores input data in Redis using the random key and returns the key.
